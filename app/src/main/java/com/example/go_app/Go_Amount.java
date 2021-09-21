26310d7f68239691ac6f8fe6cef2fa017d8c5f4d
Chanul Gunathilake <87206051+Chanuldg@users.noreply.github.com>
2021-09-21 14:21:03 +0530

    Add Amount and User id page and other stuffs(images and fonts)

--- app/src/main/java/com/example/go_app/Go_Amount.java
+++ app/src/main/java/com/example/go_app/Go_Amount.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Go_Amount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_go_user);
+        setContentView(R.layout.activity_go_amount);
     }
 }
