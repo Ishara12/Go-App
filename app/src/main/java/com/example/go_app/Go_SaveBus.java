73a004f447ea1757290e4484579873289854c1b7
Anuradha Liyanage <IT20225438@my.sliit.lk>
2021-09-26 03:21:52 +0530

    add insert function to the add_bus page.

--- app/src/main/java/com/example/go_app/Go_SaveBus.java
+++ app/src/main/java/com/example/go_app/Go_SaveBus.java
@@ -1,8 +1,10 @@
 package com.example.go_app;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Go_SaveBus extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class Go_SaveBus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_save_bus);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Search Bus");
+//        actionBar.setSubtitle("Hello");
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
