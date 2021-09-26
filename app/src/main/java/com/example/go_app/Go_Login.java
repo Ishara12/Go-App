9d5e5cc44382e3dc4b04d9a87a823666b2072806
Ishara12 <it20253394@my.sliit.lk>
2021-09-26 22:48:42 +0530

    Add_Login page

--- app/src/main/java/com/example/go_app/Go_Login.java
+++ app/src/main/java/com/example/go_app/Go_Login.java
@@ -1,10 +1,20 @@
 package com.example.go_app;
 
-import androidx.appcompat.app.ActionBar;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Go_Login extends AppCompatActivity {
 
@@ -12,6 +22,6 @@ public class Go_Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_go_login);
-
     }
 }
+
