898ee25afe6dd8675d29a51158cc07b72c7ec8a7
Chanul Gunathilake <87206051+Chanuldg@users.noreply.github.com>
2021-09-26 17:07:32 +0530

    Add gradle files

--- app/src/main/java/com/example/go_app/MainActivity.java
+++ app/src/main/java/com/example/go_app/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.go_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_go_user);
+        setContentView(R.layout.busroute_layout);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Bus List");
+        //actionBar.setSubtitle("Hello");
+        //actionBar.setIcon(R.drawable.bluebus);
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.Profile:
+                startActivity(new Intent(MainActivity.this, Go_SeatBooking.class));
+                Toast.makeText(this, "Profile selected", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.BusSearch:
+                Toast.makeText(this, "Bus Search selected", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
